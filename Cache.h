e2779d4dba59b3b6e52157e57223de39a6d36374
Deived William Jal Windows <deived.william@live.com>
2017-09-27 13:42:04 -0300

    Add files via upload

--- Cache.h
+++ Cache.h
@@ -1,32 +1,45 @@
-#ifndef CACHE_H
-#define CACHE_H
+#ifndef CACHE_H_
+#define CACHE_H_
 
 #include <stdbool.h>
 
+#define Bytes *1u
+
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 16 Bytes
+#endif
+
+#ifndef CACHE_LEVELS
+#define CACHE_LEVELS
+#endif
 
 typedef enum _Mapping
 {
-	Direct,
-	associativity	
+	Direct = 0,
+	Associativity,
+	TotalAssociativity
 }*Mapping;
 
-typedef struct _Row 
+typedef enum 
 {
-	void* block;
-	unsigned tag;
-	bool isValid;	
-}*Row;
+	LRU,
+	RANDOM
+}Subs;
 
 typedef struct _Cache
 {
-	Row* rows;
-	unsigned size;
-	bool isHit;
-	bool isMiss;
-	unsigned missCounter;
-	unsigned hitCounter;	
-}*Cache;
+	struct 
+	{
+		void* array;
+	}Block[BLOCK_SIZE];
 
+	bool cacheMiss;
+	bool cacheHit;
+	unsigned long long missCounter;
+	unsigned long long hitCounter;
+	void*(*readBlock)(unsigned long long address, struct _Cache* cache);
+	void(*writeBlock)(unsigned long long address, Subs,struct _Cache* cache);
+}*Cache;
 
 
 
